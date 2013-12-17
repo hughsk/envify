00d6a6553035c1cc7a4d7111f71e04752b45b4ab
Ben Newman <bn@cs.stanford.edu>
2013-12-17 12:45:42 -0500

    Skip parsing/transformation when process.env not present.
    Most of the changes here are just reindentation, as you can see from
    `git show -w`.

--- custom.js
+++ custom.js
@@ -1,5 +1,6 @@
 var through = require('through')
   , falafel = require('falafel')
+  , processEnvPattern = /\bprocess\.env\b/
 
 module.exports = function(env) {
   env = env || process.env || {}
@@ -13,43 +14,47 @@ module.exports = function(env) {
     return through(function(data) {
       buffer.push(data)
     }, function processFile() {
-      var output = falafel(buffer.join(''), function(node) {
-        if (!(
-          node.type === 'Identifier' &&
-          node.name === 'process' &&
-          node.parent &&
-          node.parent.property &&
-          node.parent.property.type === 'Identifier' &&
-          node.parent.property.name === 'env' &&
-          node.parent.parent &&
-          node.parent.parent.property &&
-        ( node.parent.parent.property.name ||
-          node.parent.parent.property.value ) &&
-        (!node.parent.parent.parent
-        ? true
-        :!(
-          node.parent.parent.parent.type === 'AssignmentExpression' &&
-          node.parent.parent.parent.left.type === 'MemberExpression' &&
-          node.parent.parent.parent.left.object.object &&
-          node.parent.parent.parent.left.object.object.name === 'process'
-        )))) return
+      var source = buffer.join('')
 
-        var key = node.parent.parent.property.name ||
-                  node.parent.parent.property.value
+      if (processEnvPattern.test(source)) {
+        source = falafel(source, function(node) {
+          if (!(
+            node.type === 'Identifier' &&
+            node.name === 'process' &&
+            node.parent &&
+            node.parent.property &&
+            node.parent.property.type === 'Identifier' &&
+            node.parent.property.name === 'env' &&
+            node.parent.parent &&
+            node.parent.parent.property &&
+          ( node.parent.parent.property.name ||
+            node.parent.parent.property.value ) &&
+          (!node.parent.parent.parent
+          ? true
+          :!(
+            node.parent.parent.parent.type === 'AssignmentExpression' &&
+            node.parent.parent.parent.left.type === 'MemberExpression' &&
+            node.parent.parent.parent.left.object.object &&
+            node.parent.parent.parent.left.object.object.name === 'process'
+          )))) return
 
-        if (!(key in env)) return
+          var key = node.parent.parent.property.name ||
+                    node.parent.parent.property.value
 
-        node.parent.parent.update(
-          env[key] ? JSON.stringify(env[key]) :
-          env[key] === false ? 'false'        :
-          env[key] === null ? 'null'          :
-          env[key] === '' ? '""'              :
-          env[key] === 0 ? '0'                :
-          'undefined'
-        )
-      })
+          if (!(key in env)) return
 
-      this.queue(String(output))
+          node.parent.parent.update(
+            env[key] ? JSON.stringify(env[key]) :
+            env[key] === false ? 'false'        :
+            env[key] === null ? 'null'          :
+            env[key] === '' ? '""'              :
+            env[key] === 0 ? '0'                :
+            'undefined'
+          )
+        })
+      }
+
+      this.queue(String(source))
       this.queue(null)
     })
   }
