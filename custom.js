cd5bcd5d034fa2ca35737293fcf0b14434f5775d
Andres Suarez <zertosh@gmail.com>
2015-06-12 23:11:15 -0400

    switch from jstransform to acorn

--- custom.js
+++ custom.js
@@ -1,17 +1,18 @@
 var through = require('through')
-  , jstransform = require('jstransform')
-  , createVisitors = require('./visitors')
+  , acorn = require('acorn')
+  , walk = require('acorn/dist/walk')
 
+var acornOpts = {ecmaVersion: 6, ranges: true, allowHashBang: true}
+var jsonFile = /\.json$/
 var processEnvPattern = /\bprocess\.env\b/
 
 module.exports = function(rootEnv) {
   rootEnv = rootEnv || process.env || {}
 
   return function envify(file, argv) {
-    if (/\.json$/.test(file)) return through()
+    if (jsonFile.test(file)) return through()
 
     var buffer = []
-    argv = argv || {}
 
     return through(write, flush)
 
@@ -23,9 +24,12 @@ module.exports = function(rootEnv) {
       var source = buffer.join('')
 
       if (processEnvPattern.test(source)) {
+        var envs = argv ? [argv, rootEnv] : [rootEnv];
+        var purge = envs.some(function(env) {
+          return env._ && env._.indexOf('purge') !== -1
+        })
         try {
-          var visitors = createVisitors([argv, rootEnv])
-          source = jstransform.transform(visitors, source).code
+          source = replacer(source, envs, purge);
         } catch(err) {
           return this.emit('error', err)
         }
@@ -35,4 +39,60 @@ module.exports = function(rootEnv) {
       this.queue(null)
     }
   }
+}
+
+function replacer(source, envs, purge) {
+  var ast = acorn.parse(source, acornOpts)
+
+  var buffer = ''
+  var position = 0
+
+  walk.ancestor(ast, {
+    MemberExpression: function(node, state) {
+      var parent = state[state.length - 2]
+
+      if (!(
+        node.type === 'MemberExpression'
+        && !(parent.type === 'AssignmentExpression' && parent.left === node)
+        && node.property.type === (node.computed ? 'Literal' : 'Identifier')
+        && node.object.computed === false
+        && node.object.type === 'MemberExpression'
+        && node.object.object.type === 'Identifier'
+        && node.object.object.name === 'process'
+        && node.object.property.type === 'Identifier'
+        && node.object.property.name === 'env'
+      )) return
+
+      var key = node.property.name || node.property.value
+
+      for (var i = 0; i < envs.length; i++) {
+        var value = envs[i][key]
+        if (value !== undefined) {
+          replaceEnv(node, value)
+          return
+        }
+      }
+
+      if (purge) {
+        replaceEnv(node, undefined)
+      }
+    }
+  })
+
+  function replaceEnv(node, value) {
+    if (position > node.start) {
+      throw new Error('envify replacer tried to go backwards')
+    }
+    buffer += source.substring(position, node.start)
+    buffer += JSON.stringify(value)
+    position = node.end
+  }
+
+  if (position === 0) {
+    buffer = source
+  } else {
+    buffer += source.substring(position)
+  }
+
+  return buffer
 }
